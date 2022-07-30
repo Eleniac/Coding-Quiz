40a6498438e2494261237ee1a209dc35b4fecab9
Len87G <Eleniac@github.com>
2022-07-30 11:47:44 -0600

    made sure all variables were present in HTML and CSS documents removed some

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,8 @@
 //buttons Id's
-var time = document.querySelector (".timeEl");
 var startBtn = document.querySelector ("#start");
 var choices = document.querySelector(".choices")
-var questionsArray=document.querySelector("#question-text")
-var timeleft= 45;
+var questionsArray=document.querySelector("#questionContent")
+var endGame = document.querySelector("#end-screen")
 
 var choicesArray = ["A function", "An emoji", "List of Values", "Programming Language","An element", "A computer game", "A kind of coffee", "Programming Language","A variable", "used for all elements", "List of Values", "A tag","A program", "An attachment on a shoe", "List of Values", "A front-end framework"]
 
@@ -49,13 +48,13 @@ startBtn.addEventListener('click', codingStart)
 
 //Function #2 getting the questions and choices to display (pulling questions and one index)
 function questionsAppear() {
-questions.style.display = "block";
+// questions.style.display = "block";
    questionsArray.textContent=questionsArray[0]
-   startBtn.textContent=choicesArray[0-3]
+   choicesArray.textContent=choicesArray[0-3]
     choicesArray++
   
 }
-questions.addEventListener('click', questionsAppear) 
+choicesArray.addEventListener('click', questionsAppear) 
 
 
 //Function #3 user selects choice and determines if it is right or wrong (where are you in the index) continue to next question through function 3 or end game
